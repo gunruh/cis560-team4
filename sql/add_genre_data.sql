fc5307f4e081b8560993a1336e4b9755cdae93c5
Grant Unruh <grantunruh640@gmail.com>
2015-12-11 14:33:27 -0600

    removing database from github

---
+++ sql/add_genre_data.sql
@@ -0,0 +1,3 @@
+update movies
+set unknown= , Action=, Adventure, Animation, Children, Comedy, Crime, Documentary, Drama, Fantasy, Film_Noir, Horror, Musical, Mystery, Romance, Sci_Fi, Thriller, War, Western
+where movies.title
